f8f53805166e1925af71e7774926473c9c863965
Sumi04239 <rn5txe158@gmail.com>
2021-01-04 20:18:59 +0900

    ログイン、新規登録実装

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+with_options presence: true do
+  validates :nickname
+  validates :birthday
+  validates :email
+  validates :birthday
+  validates :password, format: { with: /\A[a-zA-Z0-9]+\z/, message: 'Password Include both letters and numbers' },
+                       length: { minimum: 6 }
+end
+
+end

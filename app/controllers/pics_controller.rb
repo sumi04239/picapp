fa133e7499c07d3e53e5759ee8f9fb8c73a633a9
Sumi04239 <rn5txe158@gmail.com>
2021-01-20 15:43:27 +0900

    README変更

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -2,12 +2,12 @@ class PicsController < ApplicationController
   before_action :authenticate_user!, only: [:show]
 
   def index
-    @albums = Album.all
+    @albums = Album.all.order(created_at: :desc)
   end
 
   def show
     albums = Album.all
     user = User.find(params[:id])
-    @albums = user.albums
+    @albums = user.albums.order(created_at: :desc)
   end
 end
